9da551f8211e79837f8fe169ffac00e6105b10be
rahul9890 <rkumbhar911@gmail.com>
2025-03-11 22:38:51 +0530

    added local storage and login

--- src/LoginAndRegister/Login.jsx
+++ src/LoginAndRegister/Login.jsx
@@ -1,19 +1,49 @@
-import React from "react";
+import { React, useState } from "react";
 
 export default function Login() {
-    return (
-        <>
-            <form>
-                <h2>Login</h2>
-                Email:
-                <input type="text" name="email" id="email" />
-                <br />
-                Password: <input type="password" name="password" id="password" />
-                <br />
-                <button type="submit" value="Login">
-                    Login
-                </button>
-            </form>
-        </>
-    );
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLoginClick = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (
+      storedUser &&
+      storedUser.email == email &&
+      storedUser.password == password
+    ) {
+      alert("Login Success");
+    } else {
+      alert("invalid password");
+    }
+  };
+
+  return (
+    <>
+      <form>
+        <h2>Login</h2>
+        Email:
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        Password:{" "}
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button onClick={handleLoginClick}>Login</button>
+      </form>
+    </>
+  );
 }
--- src/LoginAndRegister/Register.jsx
+++ src/LoginAndRegister/Register.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
+
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleRegisterClick = () => {
+        if (email && password) {
+            localStorage.setItem("user", JSON.stringify({ email, password }));
+            alert("registration successful");
+            navigate("/");
+        } else {
+            alert("Please enter email and passoword")
+        }
+    }
+
+
+
   return (
     <>
       <h2>Register</h2>
@@ -9,15 +27,13 @@ export default function Register() {
         <input type="text" />
         <br />
         Email:
-        <input type="email" />
+        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
         <br />
         Password:
-        <input type="password" name="password" id="password" />
+        <input type="password" name="password" id="password" onChange={(e)=>setPassword(e.target.value)}/>
         <br />
-        Confirm Password:
-        <input type="password" name="confirmPassword" id="confirmPassword" />
-        <br />
-        <input type="submit" value="Register" />
+        
+        <input type="submit" value="Register" onClick={handleRegisterClick} />
       </form>
     </>
   );
